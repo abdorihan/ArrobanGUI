8a80031df593f8632c51e667ad3d0aca2474bc46
abdorihan <abdorihan@yahoo.com>
2022-03-21 18:09:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import HomePage from './Screens/index';
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+
+export default App;
